cf05bdb3eeb73ce091868a4e755ca1e0101d4c1e
Galiwango Ananiya <galiwango101@gmail.com>
2021-05-09 23:09:56 +0300

    Add capability to drag and drop to a specific position.

--- src/index.js
+++ src/index.js
@@ -14,7 +14,26 @@ draggables.forEach(draggable => {
 containers.forEach(container => {
   container.addEventListener('dragover', (e) => {
     e.preventDefault();
+    const afterElement = getDragAfterElement(container, e.clientY);
     const draggable = document.querySelector('.dragging');
-    container.appendChild(draggable);
+    if(afterElement == null) {
+      container.appendChild(draggable); 
+    } else {
+      container.insertBefore(draggable, afterElement)
+    }
   })
 })
+
+function getDragAfterElement(container, y) {
+  const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')];
+  return draggableElements.reduce((closest, child) => {
+    const box = child.getBoundingClientRect();
+    const offset = y -box.top - box.height / 2;
+    if(offset < 0 && offset > closest.offset){
+      return {offset: offset, element: child};
+    }
+    else {
+      return closest;
+    }
+  }, {offset: Number.NEGATIVE_INFINITY}).element
+}
